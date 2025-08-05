6c8d2c7489d6863b8fac7fcf9e210e0ce2c67dd5
Vietbx23 <Vietbx23@gmail.com>
2025-08-05 16:09:28 +0800

    xuanviet: update app.py

--- app.py
+++ app.py
@@ -90,5 +90,7 @@ def upload_files():
     return jsonify(created_urls)
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     print("🚀 Starting Flask server...")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=port)
+
